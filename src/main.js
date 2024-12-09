c77ab495d2fb5fa6f90062d510b5a35530c2060e
Andrey9019 <1234andrey5678@gmail.com>
2024-12-09 23:07:54 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import { AboutPage } from "./page/AboutPage";
+// import { ProjectsPage } from "./page/ProjectsPage";
+// import { ContactsPage } from "./page/ContactsPage";
+// import ScrollToTop from "ui/ScrollToTop";
+
+// import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root"))
+  .render
+  //   <React.StrictMode>
+  //     <Router>
+  //       <div className="flex flex-col min-h-screen">
+  //         <ScrollToTop />
+  //         <div className="flex-grow">
+  //           <Routes>
+  //             <Route path="/" element={<AboutPage />} />
+  //             <Route path="/projects" element={<ProjectsPage />} />
+  //             <Route path="/contacts" element={<ContactsPage />} />
+  //           </Routes>
+  //         </div>
+  //       </div>
+  //     </Router>
+  //   </React.StrictMode>
+  ();
